5c510ae5c746209b67801c164d15d9bceaabcd15
Narayana <ravurinarayana0123@gmail.com>
2022-12-18 22:53:33 +0530

    Added a new line

---
+++ Non_Primitive_Data_Type.js
@@ -0,0 +1,11 @@
+//Objects are the non primitive data types in Java Script
+//Objects are created as below
+
+const obj1 = {
+    name:"Narayana",
+    empid:2138,
+    company:"healthasyst"
+}
+
+console.log(obj1.name);
+console.log(obj1.empid);
---
+++ Primitive_Data_Types.js
@@ -0,0 +1,13 @@
+//NN BB SS U
+let a = null;
+let b = 234;
+
+let c = true;
+let d = BigInt("1123")+BigInt("123");
+
+let e = "Harry";
+let f = Symbol("symbol");
+
+let g = undefined;
+
+console.log(a,b,c,d,e,f,g);
